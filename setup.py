472e407b91146219db1dbeaeeac325cb34775c06
Qiangning Hong <hongqn@douban.com>
2012-02-24 14:26:36 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys, os
+from setuptools import setup, find_packages
+
+install_requires = [
+    'umysql',
+]
+
+setup(
+    name = 'oursql',
+    description = "MySQLdb compatible wrapper for ultramysql",
+    long_description = open(os.path.join(os.path.dirname(__file__),
+                                         'README.rst')).read(),
+    version = '0.1',
+    packages = find_packages(exclude=['examples', 'tests']),
+    install_requires = install_requires,
+    author = "Qiangning Hong",
+    author_email = "hongqn@douban.com",
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+)
