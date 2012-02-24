fa1658fb9c9a7b178c5a2f7668c79742beec4fb5
Qiangning Hong <hongqn@douban.com>
2012-02-24 22:03:54 +0800

    refactor

--- oursql/cursors.py
+++ oursql/cursors.py
@@ -5,10 +5,14 @@ from .utils import setdocstring
 class Cursor(pymysql.cursors.Cursor):
     @setdocstring(pymysql.cursors.Cursor)
     def execute(self, query, args=None):
-        conn = self._get_db()
-
         if args is None:
             args = ()
         elif not isinstance(args, (tuple, list, dict)):
             args = (args,)
+
+        return self._query(query, args)
+
+    def _query(self, query, args):
+        conn = self._get_db()
         return conn.query(query, args)
+
