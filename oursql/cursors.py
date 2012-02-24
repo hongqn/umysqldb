4dc5e2bcf7160656039a4a6e422e07f915779e5c
Qiangning Hong <hongqn@douban.com>
2012-02-24 22:53:12 +0800

    pass 23 tests

--- oursql/cursors.py
+++ oursql/cursors.py
@@ -14,12 +14,16 @@ class Cursor(pymysql.cursors.Cursor):
 
     def _query(self, query, args):
         conn = self._get_db()
-        result = conn.query(query, args)
+        self._result = result = conn.query(query, args)
         if isinstance(result, tuple):
             self.rowcount = result[0]
+            self.description = None
             self._rows = ()
         else:
             self.rowcount = len(result.rows)
+            # fill field to 7-item tuple to meet DBAPI spec
+            self.description = tuple(f + (None,) * (7 - len(f))
+                                     for f in result.fields)
             self._rows = result.rows
         self.rownumber = 0
         return self.rowcount
