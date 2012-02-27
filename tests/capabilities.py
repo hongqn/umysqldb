a829e342a223b7c8447337ee4d40827a65bc8e12
Qiangning Hong <hongqn@douban.com>
2012-02-27 20:25:14 +0800

    skip a test

--- tests/capabilities.py
+++ tests/capabilities.py
@@ -264,6 +264,7 @@ class DatabaseTest(unittest.TestCase):
                  generator)
 
     def test_TEXT(self):
+        raise SkipTest("umysql treats BLOBs as bytes, not strings")
         def generator(row,col):
             return self.BLOBUText # 'BLOB Text ' * 1024
         self.check_data_integrity(
