dfbd463b991912926f36f368be3fb926224801fe
Qiangning Hong <hongqn@douban.com>
2012-02-28 18:06:54 +0800

    remove an invalid test case

--- tests/thirdparty/test_pymysql/test_issues.py
+++ tests/thirdparty/test_pymysql/test_issues.py
@@ -108,14 +108,6 @@ KEY (`station`,`dh`,`echeance`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;""")
         finally:
             cur.execute("drop table issue13")
 
-    def test_issue_14(self):
-        """ typo in converters.py """
-        self.assertEqual('1', oursql.converters.escape_item(1, "utf8"))
-        self.assertEqual('1', oursql.converters.escape_item(1L, "utf8"))
-
-        self.assertEqual('1', oursql.converters.escape_object(1))
-        self.assertEqual('1', oursql.converters.escape_object(1L))
-
     def test_issue_15(self):
         """ query should be expanded before perform character encoding """
         conn = self.connections[0]
